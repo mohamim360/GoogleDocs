539e9f5d91618f2dce6e60676b89f12996d75230
mohamim360 <mohamim360@gmail.com>
2025-06-16 11:55:46 +0600

    Refactor server setup

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const http = require('http');
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
 
-export default app;
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
